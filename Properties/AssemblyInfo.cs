7cf7134e2aa2f62073e303491655c7bbc5914a10
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2021-04-18 11:09:52 -0700

    cambios en las propiedades "AssemblyTitle" y "AssemblyProduct"

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FormPrueba")]
+[assembly: AssemblyTitle("Axolotl_Agenda")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FormPrueba")]
+[assembly: AssemblyProduct("Axolotl_Agenda")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
